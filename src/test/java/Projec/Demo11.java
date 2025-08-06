6af839032b9bbd4dfce07ae25ba90b7cb076d94d
abhij <abhijithraj221@gmail.com>
2025-08-06 20:36:55 +0530

    Committed

--- src/test/java/Projec/Demo11.java
+++ src/test/java/Projec/Demo11.java
@@ -1,10 +1,10 @@
-package Demo2;
+package Projec;
 
-public class Demo21 {
+public class Demo11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hi");
+System.out.println("hi");
 	}
 
 }
