8cf061a6d99cd1ecc8327f02cc6c0014d01c6231
abhij <abhijithraj221@gmail.com>
2025-08-06 20:22:32 +0530

    Committed

--- src/test/java/Practc/Practice1.java
+++ src/test/java/Practc/Practice1.java
@@ -1,10 +1,10 @@
-package Pract;
+package Practc;
 
 public class Practice1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("KGF");
+    System.out.println("KGF");
 	}
 
 }
